0b15cb6ca1cee9c5cb1ef62c725962015147c4dc
Ava DeCroix <adecroix@gmail.com>
2022-10-04 16:49:11 -0400

    brute_deepmind.py

--- brute_deepmind.py
+++ brute_deepmind.py
@@ -275,7 +275,7 @@ def check_against_answer_key(code_answer, given_answer):
         return 1
 
     #accounts for the case where there is no answer key
-    elif given_answer == 'NA':
+    elif given_answer == '?':
         return 0
 
     elif code_answer != given_answer:
